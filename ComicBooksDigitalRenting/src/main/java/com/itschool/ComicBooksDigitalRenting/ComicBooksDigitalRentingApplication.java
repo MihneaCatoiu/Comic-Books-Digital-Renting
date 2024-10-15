7e5b9026bd29cade593e269a461e0700e2d613d2
MihneaCatoiu <mihneaaureliancatoiu@gmail.com>
2024-10-15 20:01:50 +0300

    Initial commit

---
+++ ComicBooksDigitalRenting/src/main/java/com/itschool/ComicBooksDigitalRenting/ComicBooksDigitalRentingApplication.java
@@ -0,0 +1,13 @@
+package com.itschool.ComicBooksDigitalRenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicBooksDigitalRentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicBooksDigitalRentingApplication.class, args);
+	}
+
+}
---
+++ ComicBooksDigitalRenting/src/test/java/com/itschool/ComicBooksDigitalRenting/ComicBooksDigitalRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itschool.ComicBooksDigitalRenting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicBooksDigitalRentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
